746b510dc329935ec60f66cc8acaa3524f291f8c
BuiThanhPhuoc <buithanhphuoc27092004@gmail.com>
2026-03-11 09:50:17 +0700

    A commit thu 1

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+// Cài đặt EJS làm view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Phục vụ file tĩnh (CSS, Images)
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000, () => console.log('Server is running...'));
+
+// Route chính render trang chủ
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => console.log('Server chạy tại http://localhost:3000'));
